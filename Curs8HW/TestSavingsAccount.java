9a43b8c131a31f71c42676d8dace54d2a8a27889
Augustin Cibotari <augustus.apso@gmail.com>
2023-06-10 17:27:51 +0300

    Curs8HW

---
+++ Curs8HW/TestSavingsAccount.java
@@ -0,0 +1,93 @@
+import java.util.Scanner;
+
+class Customer {
+    private String name;
+    private String address;
+    private String email;
+
+    public Customer(String name, String address, String email) {
+        this.name = name;
+        this.address = address;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+class InsuficientFundsException extends Exception {
+    public InsuficientFundsException() {
+        super("Insuficient funds for transaction");
+    }
+}
+
+class Account {
+    private int accountNumber;
+    public double balance;
+    public Customer accountOwner;
+
+    public void withdrawMoney(int amount) {
+        try {
+            throw new InsuficientFundsException();
+        } catch (InsuficientFundsException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    // public int getAccountNumber() {
+    // return accountNumber;
+    // }
+}
+
+class SavingsAccount extends Account {
+    private static int accountNumbers = 0;
+
+    public SavingsAccount(Customer accountOwner, double balance) {
+        this.accountOwner = accountOwner;
+        this.balance = balance;
+        this.setAccountNumber(++accountNumbers);
+    }
+
+    @Override
+    public void withdrawMoney(int amount) {
+        try {
+            if (amount > this.balance) {
+                throw new InsuficientFundsException();
+            } else {
+                this.balance -= amount;
+                System.out.println("Please pick up your money!");
+                System.out.println("New balance: " + this.balance);
+                System.out.println("Thank you for using our ATM");
+            }
+        } catch (InsuficientFundsException eFundsException) {
+            System.out.println("The amount you entered is greater than the available balance: " + balance);
+            System.out.println("Thank you for using our ATM");
+            System.out.println(eFundsException);
+            ;
+        }
+    }
+
+}
+
+class TestSavingsAccount {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        Customer augustin = new Customer("Augustin", "Bucharest", "ag@test.com");
+        SavingsAccount augustinSavingsAccount = new SavingsAccount(augustin, 100.5);
+        System.out.println("Hi " + augustin.getName());
+
+        // System.out.println("Account number:" +
+        // augustinSavingsAccount.getAccountNumber());
+
+        System.out.print("Please enter the amount to withdraw: ");
+        augustinSavingsAccount.withdrawMoney(input.nextInt());
+
+        input.close();
+    }
+}
